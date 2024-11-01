c03f846adac1b3e035b522f7fa3fd81ad81ce764
VeenaRoy23 <127951652+VeenaRoy23@users.noreply.github.com>
2024-11-01 11:51:36 +0530

    Update charcount.asm

--- charcount.asm
+++ charcount.asm
@@ -1,10 +1,6 @@
-section .data
-    msg1 db 'Enter the string:', 0xa
-    len1 equ $ - msg1
-    msg2 db 'Enter the character:', 0xa
-    len2 equ $ - msg2
-    msg3 db 'Character count:'
-    len3 equ $ - msg3
+ section .data
+    msg1 db "String",0xa
+    msg2 db "Character",0xa
     
 section .bss
     str1 resb 100
@@ -13,79 +9,58 @@ section .bss
     
 section .text
     global _start
-    
+
 _start:
-    ; Print message to enter the string
-    mov eax, 4
-    mov ebx, 1
-    lea ecx, [msg1]
-    mov edx, len1
+    mov eax,4
+    mov ebx,1
+    mov ecx,msg1
+    mov edx,7
     int 0x80
     
-    ; Read the input string
-    mov eax, 3
-    mov ebx, 0
-    lea ecx, [str1]
-    mov edx, 100
+    mov eax,3
+    mov ebx,0
+    mov ecx,str1
+    mov edx,100
+    int 0x80
+    mov byte[str1+eax-1],0
+    
+    mov eax,4
+    mov ebx,1
+    mov ecx,msg2
+    mov edx,10
     int 0x80
     
-    ; Null-terminate the input string
-    mov byte [str1 + eax - 1], 0
-    
-    ; Print message to enter the character
-    mov eax, 4
-    mov ebx, 1
-    lea ecx, [msg2]
-    mov edx, len2
+    mov eax,3
+    mov ebx,0
+    mov ecx,chr
+    mov edx,1
     int 0x80
     
-    ; Read the character
-    mov eax, 3
-    mov ebx, 0
-    lea ecx, [chr]
-    mov edx, 1
+    mov esi,str1
+    mov dl,[chr]
+    xor ecx,ecx
+    
+    loop1:mov al,[esi]
+    cmp al,0
+    je end
+    cmp al,dl
+    jne eq
+    inc ecx
+    inc esi
+    jmp loop1
+    
+    eq:inc esi
+    jmp loop1
+    
+    end:mov eax,ecx
+    add eax,'0'
+    mov [count],eax
+    
+    mov eax,4
+    mov ebx,1
+    mov ecx,count
+    mov edx,10
     int 0x80
     
-    ; Initialize counters
-    xor ecx, ecx  ; Count of characters matching
-    lea esi, [str1] ; Source string pointer
-    mov al, [chr] ; Character to count
-
-count_loop:
-    mov bl, [esi]   ; Load current character from string
-    cmp bl, 0       ; End of string check
-    je done
-    cmp bl, al      ; Compare with target character
-    jne next_char
-    inc ecx         ; Increment count if match
-next_char:
-    inc esi         ; Move to the next character
-    jmp count_loop
-    
-done:
-    ; Store the count in `count`
-    mov [count], ecx
-    
-    ; Print character count message
-    mov eax, 4
-    mov ebx, 1
-    lea ecx, [msg3]
-    mov edx, len3
+    mov eax,1
     int 0x80
-    
-    ; Convert the count to ASCII and print it
-    mov eax, [count]
-    add eax, '0'    ; Convert to ASCII
-    mov [count], al ; Store the ASCII character
-    
-    mov eax, 4
-    mov ebx, 1
-    lea ecx, [count]
-    mov edx, 1      ; Print single character
-    int 0x80
-    
-    ; Exit the program
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
-
