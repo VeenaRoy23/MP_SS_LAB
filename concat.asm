804dd637060a9d74e0000c99b525a18d672512c3
VeenaRoy23 <127951652+VeenaRoy23@users.noreply.github.com>
2024-11-01 11:24:53 +0530

    concat.asm

--- concat.asm
+++ concat.asm
@@ -1,79 +1,76 @@
 section .data
-	msg1 db 'Enter string1',0xa
-	len1 equ $-msg1
-	msg2 db 'Enter string2',0xa
-	len2 equ $-msg2
-	
+    msg1 db "Enter first string",0xa
+    msg2 db "Enter second string",0xa
+    msg3 db "string",0xa
+    
 section .bss
-	str1 resb 15
-	str2 resb 15
-	str3 resb 70
-	
+    str1 resb 100
+    str2 resb 100
+    str3 resb 200
+    
 section .text
-	global _start
-	
+    global _start
+    
 _start:
-	MOV eax,4
-	MOV ebx,1
-	MOV ecx,msg1
-	MOV edx,len1
-	INT 0x80
-	
-	MOV eax,3
-	MOV ebx,0
-	MOV ecx,str1
-	MOV edx,15
-	INT 0x80
-	
-	MOV eax,4
-	MOV ebx,1
-	MOV ecx,msg2
-	MOV edx,len2
-	INT 0x80
-	
-	MOV eax,3
-	MOV ebx,0
-	MOV ecx,str2
-	MOV edx,15
-	INT 0x80
-	
-	mov byte[str1+eax-1],0
-	mov byte[str2+eax-1],0
-	
-	MOV eax,str1
-	MOV ebx,str3
-	
-loop1:
-	MOV dl,[eax]
-	MOV [ebx],dl
-	inc eax
-	inc ebx
-	test dl,dl
-	jnz loop1
-	
-MOV eax,str2
-	
-loop2:
-	MOV dl,[eax]
-	MOV [ebx],dl
-	inc eax
-	inc ebx
-	test dl,dl
-	jnz loop2
-	
-MOV eax,4
-MOV ebx,1
-MOV ecx,str3
-mov edx,edx
-sub edx,str3
-INT 0x80
+    mov eax,4
+    mov ebx,1
+    mov ecx,msg1
+    mov edx,19
+    int 0x80
+    
+    mov eax,3
+    mov ebx,0
+    mov ecx,str1
+    mov edx,100
+    int 0x80
+    mov byte[str1+eax-1],0
+    
+    mov eax,4
+    mov ebx,1
+    mov ecx,msg2
+    mov edx,20
+    int 0x80
+    
+    mov eax,3
+    mov ebx,0
+    mov ecx,str2
+    mov edx,100
+    int 0x80
+    mov byte[str2+eax-1],0
+    
+    mov eax,str1
+    mov ebx,str3
+    
+    loop1:mov dl,[eax]
+    test dl,dl
+    jz next
+    mov [ebx],dl
+    inc eax
+    inc ebx
+    jmp loop1
 
-MOV eax,1
-xor ebx,ebx
-INT 0x80
-
-
-	
-	
-	
-	
+    next:mov eax,str2
+    
+    loop2:mov dl,[eax]
+    test dl,dl
+    jz end
+    mov [ebx],dl
+    inc eax
+    inc ebx
+    jmp loop2
+    
+    end:
+    mov eax,4
+    mov ebx,1
+    mov ecx,msg3
+    mov edx,7
+    int 0x80
+    
+    mov eax,4
+    mov ebx,1
+    mov ecx,str3
+    mov edx,100
+    int 0x80
+    
+    mov eax,1
+    int 0x80
