4b02e63078b0c52499a9f27b390b0b61821d79be
VeenaRoy23 <127951652+VeenaRoy23@users.noreply.github.com>
2024-11-03 10:48:27 +0530

    Update passTwo.c

--- cycle4/passTwo.c
+++ cycle4/passTwo.c
@@ -1,115 +1,105 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-char opcode[10], operand[10], label[10], hexcode[10], line[50];
-int locctr, start, size,end_locctr;
-FILE *fp1, *fp2, *fp3, *fp4;
-
-struct SYMTAB {
-    char label[10];
+struct optab{
+    char opcode[10];
+    char hexcode[10];
+}OT[30];
+struct symtab{
+    char symbol[10];
     int addr;
-} ST[30];
+}ST[30];
 
-struct OPTAB {
-    char opcode[10], hexcode[10];
-} OT[30];
+int op_length,size,locctr,end_locctr,sym_length,start,instruction_length,is_found;
+FILE*fp1,*fp2,*fp3,*fp4;
+char label[10],opcode[10],hexcode[10],operand[10],line[50];
 
-void read_SYMTAB() {
-    int i = 0;
-    while (fscanf(fp2, "%s %d", ST[i].label, &ST[i].addr) != EOF) {
-        i++;
+void op_read(){
+    int i;
+    for(i=0;i<30;i++){
+        fscanf(fp1,"%s\t%s",OT[i].opcode,OT[i].hexcode);
     }
+    op_length=i;
 }
 
-void read_OPTAB() {
-    int i = 0;
-    while (fscanf(fp1, "%s %s", OT[i].opcode, OT[i].hexcode) != EOF) {
-        i++;
+void sym_read(){
+    int i;
+    for(i=0;i<30;i++){
+        fscanf(fp2,"%s\t%d",ST[i].symbol,&ST[i].addr);
     }
+    sym_length=i;
 }
 
-/*int get_address(char *label) {
-    for (int i = 0; i < 30; i++) {
-        if (strcmp(ST[i].label, label) == 0) {
-            return ST[i].addr;
+int get_addr(char *label){
+    for(int j=0;j<sym_length;j++){
+        if(strcmp(ST[j].symbol,operand)==0){
+            return ST[j].addr;
         }
     }
-    return -1; 
+    return -1;
 }
-*/ 
-int main() {
-    fp1 = fopen("optab.txt", "r");
-    fp2 = fopen("symtab.txt", "r");
-    fp3 = fopen("intermediate.txt", "r");
-    fp4 = fopen("Output.txt", "w");
 
-    if (!fp1 || !fp2 || !fp3 || !fp4) {
-        printf("Error opening files.\n");
-        return 1;
-    }
-
-    read_OPTAB();
-    read_SYMTAB();
-
+int main(){
+    fp1=fopen("optab.txt","r");
+    fp2=fopen("symtab.txt","r");
+    fp3=fopen("intermediate.txt","r");
+    fp4=fopen("output.txt","w");
     
-    fscanf(fp3, "%s %s %x", label, opcode, &start);
+    op_read();
+    sym_read();
+    
+    fscanf(fp3,"\t%s\t%s\t%x",label,opcode,&start);
     locctr=start;
-    while(fgets(line,sizeof(line),fp3))
-    {
-    	sscanf(line,"%x%s%s%s",&locctr,label,opcode,operand);
-    	if(strcmp(opcode,"END")==0)
-    	{
-    		end_locctr=locctr;
-    		break;
-    	}
-    }
-    size=end_locctr-start;
-    	
-    fprintf(fp4, "H^%s^%06x^%06x\n", label, start, size);
-    rewind(fp3);
-	 fscanf(fp3, "%s %s %x", label, opcode, &start);
-    while (fgets(line, sizeof(line), fp3)) {
-        sscanf(line, "%x %s %s %s", &locctr, label, opcode, operand);
-        if (strcmp(opcode, "END") == 0) {
-            fprintf(fp4, "E %06x\n", start);
+    while(fgets(line,sizeof(line),fp3)){
+        sscanf(line,"%x\t%s\t%s\t%s",&locctr,label,opcode,operand);
+        if(strcmp(opcode,"END")==0){
+            end_locctr=locctr;
             break;
         }
-
-        int opcode_found = 0;
-        for (int i = 0; i < 30; i++) {
-            if (strcmp(opcode, OT[i].opcode) == 0) {
-            	for(int j=0;j<30;j++)
-            	{
-            		if(strcmp(operand,ST[j].label)==0)
-            		{
-                		fprintf(fp4, "T^%06x^%s^%d\n", locctr, OT[i].hexcode, ST[j].addr);
-                		opcode_found = 1;
-                		break;
-           	 		}
-           	 	}
-           	 }
+    }
+    size=end_locctr-start;
+    fprintf(fp4,"H^%s^%06x^%06x\n",label,start,size);
+    rewind(fp3);
+    fscanf(fp3,"%s %s %x",label,opcode,&start);
+    while(fgets(line,sizeof(line),fp3)){
+        instruction_length=0;
+        sscanf(line,"%x %s %s %s",&locctr,label,opcode,operand);
+        if(strcmp(opcode,"END")==0){
+            fprintf(fp4,"E^%06x\n",start);
+            break;
         }
-
-        if (!opcode_found) {
-           
-            if (strcmp(opcode, "WORD") == 0) {
-                fprintf(fp4, "T^%06x^%s^%06x\n", locctr, "00", atoi(operand));
-            } else if (strcmp(opcode, "BYTE") == 0) {
-                if (operand[0] == 'C' || operand[0] == 'c') {
-                    fprintf(fp4, "T^%06x^%s^%s\n", locctr, "C", operand + 3); 
-                } else {
-                    fprintf(fp4, "T^%06x^%s^%s\n", locctr, "H", operand + 3); 
+        is_found=0;
+        for(int i=0;i<op_length;i++){
+            if(strcmp(OT[i].opcode,opcode)==0){
+                int address=get_addr(operand);
+                if(address!=-1){
+                    instruction_length=6;
+                    fprintf(fp4,"T^%06x^%02x^%s^%06x\n",locctr,instruction_length,OT[i].hexcode,address);
+                }
+                is_found=1;
+                break;
+            }
+        }
+        if(!is_found){
+            if(strcmp(opcode,"WORD")==0){
+                instruction_length=6;
+                fprintf(fp4,"T^%06x^%02x^%s^%06x\n",locctr,instruction_length,"00",atoi(operand));
+            }
+            else if(strcmp(opcode,"BYTE")==0){
+                if(operand[0]=='C'||operand[0]=='c'){
+                    instruction_length=strlen(operand)-3;
+                    fprintf(fp4,"T^%06x^%02x^%s^%s\n",locctr,instruction_length,"C",operand+2);
+                }
+                else{
+                    instruction_length=(strlen(operand)-3)/2;
+                    fprintf(fp4,"T^%06x^%02x^%s^%s\n",locctr,instruction_length,"H",operand+2);
                 }
             }
         }
     }
-
     fclose(fp1);
     fclose(fp2);
     fclose(fp3);
     fclose(fp4);
-
-    return 0;
 }
-
